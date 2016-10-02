cd75527fb99a5d42e1701e024ac4b30a7bdb9b8f
nan20 <842773043@qq.com>
2016-10-02 15:51:45 +0800

    添加工程
    1

---
+++ GitTest1/GitTest1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GitTest1
+//
+//  Created by 黄文浩 on 16/10/2.
+//  Copyright © 2016年 huangwenhao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
